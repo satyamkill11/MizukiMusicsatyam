d20231276cf34a19ace3b0ced091c61a924d9429
satyamkill11 <69784272+satyamkill11@users.noreply.github.com>
2023-07-07 23:59:49 +0530

    Update help.py

--- SankiMusic/utilities/inline/help.py
+++ SankiMusic/utilities/inline/help.py
@@ -16,7 +16,7 @@ def help_pannel(_, START: Union[bool, int] = None):
         ),
         InlineKeyboardButton(
             text="ᴍᴏʀᴇ",
-            url=f"t.me/iamkaal",
+            url=f"t.me/chatbotsupportin",
         ),
         InlineKeyboardButton(
             text="ᴄʟᴏsᴇ", callback_data=f"close"
